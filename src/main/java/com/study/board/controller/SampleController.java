82229056906e77f055335f5bca7da039d2eccb17
kimmin <ksd2521@nate.com>
2022-12-05 17:34:31 +0900

    머스태치 실행 test

--- src/main/java/com/study/board/controller/SampleController.java
+++ src/main/java/com/study/board/controller/SampleController.java
@@ -1,7 +1,8 @@
 package com.study.board.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class SampleController {
@@ -9,6 +10,24 @@ public class SampleController {
     @GetMapping("/sample")
         public String testSample() {
             return "test";
+    }
 
+
+    @RequestMapping(value = "loginInput", method = RequestMethod.GET)
+    public String helloWorld() {
+        return "loginInput";
+    }
+
+    @PostMapping("/loginOutput")
+    public String loginOutput (@RequestParam(value = "name", required = false) String name,
+                               @RequestParam String password, Model model) {
+
+        System.out.println(name);
+        System.out.println(password);
+
+        model.addAttribute("name", name);
+        model.addAttribute("password", password);
+
+        return "loginOutput";
     }
 }
